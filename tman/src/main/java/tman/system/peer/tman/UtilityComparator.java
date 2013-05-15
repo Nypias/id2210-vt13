392c5be19d6430afef6969e9915a7df296044137
George E. Kallergis <kallergis.george@gmail.com>
2013-05-15 15:17:31 +0200

    Gradient fixes

--- tman/src/main/java/tman/system/peer/tman/UtilityComparator.java
+++ tman/src/main/java/tman/system/peer/tman/UtilityComparator.java
@@ -7,16 +7,32 @@ import java.util.Comparator;
 
 public class UtilityComparator implements Comparator<PeerAddress>
 {
-    private PeerAddress baseNode;
+    private BigInteger utilityBN;
     
     public UtilityComparator(PeerAddress baseNode) {
-        this.baseNode = baseNode;
+        this.utilityBN = baseNode.getPeerId();
     }
     
+    /**
+     * Compare utility values of two nodes
+     * @param a
+     * @param b
+     * @return 
+     */
     @Override
     public int compare(PeerAddress a, PeerAddress b) {
-        BigInteger distanceA = (a.getPeerId().subtract(baseNode.getPeerId())).abs();
-        BigInteger distanceB = (b.getPeerId().subtract(baseNode.getPeerId())).abs();
+        BigInteger utilityA = a.getPeerId();
+        BigInteger utilityB = b.getPeerId();
+        
+        // First gradient condition\
+        if(utilityA.compareTo(utilityBN) == 1 && utilityB.compareTo(utilityBN) == -1) {
+            return 1;
+        }
+        
+        // Second gradient condition
+        BigInteger distanceA = (utilityA.subtract(utilityBN)).abs();
+        BigInteger distanceB = (utilityB.subtract(utilityBN)).abs();
+        
         return (-1) * distanceA.compareTo(distanceB);
     }
 }
